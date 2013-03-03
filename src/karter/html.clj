d2319ab2ddaa190de99f1552a7710a23d6501242
rod <rod@pu-gh.com>
2013-03-03 17:26:29 +0000

    age now displayed

--- src/karter/html.clj
+++ src/karter/html.clj
@@ -12,12 +12,19 @@
              (set-attr :alt login)
              (set-attr :title login)))
 
+(defn div
+  "Divide to an integer"
+  [& args]
+  (int (apply / args)))
+
 (defn age-class [pull]
-  (condp >= (:age pull)
-    1 "seconds"
-    60 "minutes"
-    (* 60 60) "hours"
-    "days"))
+  (let [one-hour 60
+        one-day (* one-hour 24)
+        age (:age pull)]
+    (condp >= age
+      one-hour [age "minutes"]
+      one-day [(div age one-hour)  "hours"]
+      [(div age one-day) "days"])))
 
 ;; Public
 ;; ------
@@ -37,7 +44,9 @@
 (defsnippet repository "pulls.html" [:.span8]
   [pulls]
   [:li] (clone-for [pull pulls]
-          [:li] (add-class (str "age-" (age-class pull)))
+          [:.age] (let [[age cls] (age-class pull)]
+                    (do-> (add-class (str "age-" cls))
+                          (content (format "%s %s" age cls))))
           [:.title] (do-> (content (:title pull))
                           (set-attr :href "asd"))
           [:.author] (author (:user pull))
